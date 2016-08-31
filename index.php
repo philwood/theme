2d5d3b1d078cc1f80c72a74480ba3a7ecba1eeec
Phil Wood <philwood128@hotmail.com>
2016-08-31 12:51:47 +0200

    Fixed Sidebar JS

--- index.php
+++ index.php
@@ -194,17 +194,19 @@
 					</div><!-- /.row -->
 
 				</div><!-- /.container -->
-			</div>
-	        <!-- /#page-content-wrapper -->
 
-	    </div>
+				<footer class="blog-footer">
+					<p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+					<p>
+						<a href="#">Back to top</a>
+					</p>
+				</footer>
 
-		<footer class="blog-footer">
-			<p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-			<p>
-				<a href="#">Back to top</a>
-			</p>
-		</footer>
+			</div><!-- /#page-content-wrapper -->
+
+	    </div><!-- /#wrapper -->
+
+				
 
 		<!-- Bootstrap core JavaScript
 		================================================== -->
@@ -213,16 +215,23 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 		<!-- Menu Toggle Script -->
 		<script>
-	    $("#menu-toggle").click(function(e) {
-	        e.preventDefault();
-	        $("#wrapper").toggleClass("toggled");
-	    });
-	    if($("#wrapper").hasClass("toggled")) {
-	    	$("div:not(#sidebar-wrapper)").click(function(e) {
-	    		e.preventDefault();
-	    		$("#wrapper").toggleClass("toggled");
-	    	});
-	    }
+
+	    $("#menu-toggle").on('click', function(e) {
+		    e.preventDefault();
+		    e.stopPropagation();
+
+		    $("#wrapper").toggleClass("toggled");
+
+		    $(document).one('click', function closeMenu (e){
+
+		        if($("#sidebar-wrapper").has(e.target).length === 0){
+		            $("#wrapper").removeClass("toggled");
+		        } else {
+		            $(document).one('click', closeMenu);
+		        }
+		    });
+		});
+
 	    </script>
 	</body>
 </html>
