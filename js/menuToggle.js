fbd30ed425c3b7cb0ddee1da1440ec3df2522f90
Phil Wood <philwood128@hotmail.com>
2016-09-02 15:56:33 +0200

    Added no scroll on toggle

--- js/menuToggle.js
+++ js/menuToggle.js
@@ -4,12 +4,12 @@ $(function() {
 	    e.preventDefault();
 	    e.stopImmediatePropagation();
 
-	    $("#wrapper").toggleClass("toggled");
+	    $("#wrapper, html, body").toggleClass("toggled");
 
 	    $(document).one('click', function closeMenu (e){
 
 	        if($("#side-nav-wrapper").has(e.target).length === 0){
-	            $("#wrapper").removeClass("toggled");
+	            $("#wrapper, html, body").removeClass("toggled");
 	        }
 	        else {
 	            $(document).one('click', closeMenu);
